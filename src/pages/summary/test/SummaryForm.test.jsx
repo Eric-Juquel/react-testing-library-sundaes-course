e629a530d638d2596558358464dd308eb8906049
Eric J <ejuquel@yahoo.fr>
2022-01-13 13:49:36 +0100

    test and code SummaryForm

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -10,33 +10,40 @@ test('button has correct initial text', () => {
 });
 
 test('checkbox has correct initial text', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-        name: 'I agree to Terms and Conditions'
-    })
-    expect(checkbox).toBeInTheDocument();
-})
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).toBeInTheDocument();
+});
 
 test('checkbox is initialy unchecked and button initialy enabled', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-        name: 'I agree to Terms and Conditions'
-    })
-    const button = screen.getByRole('button', {
-        name: 'Confirm order',
-      });
-    expect(checkbox).not.toBeChecked()
-    expect(button).toBeDisabled()
-})
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const button = screen.getByRole('button', {
+    name: 'Confirm order',
+  });
+  expect(checkbox).not.toBeChecked();
+  expect(button).toBeDisabled();
+});
 
-test('button disabled when checkbox is checked', () => {
-    const checkbox = screen.getByRole('checkbox', {
-        name: 'I agree to Terms and Conditions'
-    })
-    const button = screen.getByRole('button', {
-        name: 'Confirm order',
-      });
-    fireEvent.click(checkbox)
-    expect(checkbox).toBeChecked()
-    expect(button).toBeEnabled()
-})
+test('button enabled when checkbox is checked and disabled when unchecked', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const button = screen.getByRole('button', {
+    name: 'Confirm order',
+  });
+
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(button).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(button).toBeDisabled();
+});
+
